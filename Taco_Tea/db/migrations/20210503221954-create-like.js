f9e484493689a522fb938043ad2a95ca48c943e8
smallkids46899 <smallkids46899@protonmail.com>
2024-05-17 23:35:24 +0300

    update README.md

---
+++ Taco_Tea/db/migrations/20210503221954-create-like.js
@@ -0,0 +1,38 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Likes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "Users" },
+      },
+      post_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "Posts" },
+      },
+      emoji: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Likes");
+  },
+};
